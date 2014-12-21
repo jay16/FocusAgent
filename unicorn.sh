9218fe443885376adb14062d0df28773d5d98ab1
jay16 <jay_li@intfocus.com>
2014-12-21 21:15:42 +0800

    bash outside

--- unicorn.sh
+++ unicorn.sh
@@ -22,10 +22,18 @@ case "$1" in
     start)  
         test -d log || mkdir log
         test -d tmp || mkdir -p tmp/pids
+        echo "stop watch_dog.sh"
+        /bin/sh watch_dog.sh stop
+        echo "start unicorn"
         bundle exec ${UNICORN} -c ${CONFIG_FILE} -p ${PORT} -E ${ENVIRONMENT} -D  
+        echo "start watch_dog.sh"
+        /bin/sh watch_dog.sh start
         ;;  
     stop)  
+        echo "stop unicorn"
         kill -QUIT `cat tmp/pids/unicorn.pid`  
+        echo "stop watch_dog.sh"
+        /bin/sh watch_dog.sh stop
         ;;  
     restart|force-reload)  
         kill -USR2 `cat tmp/pids/unicorn.pid`  
